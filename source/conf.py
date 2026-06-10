234918633218e0fe4d30759bcfdf9e127f8b20b5
Peter Komaromy <78869972+peter-komaromy@users.noreply.github.com>
2026-06-10 17:29:08 +0200

    Update site content

--- source/conf.py
+++ source/conf.py
@@ -1,10 +1,5 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
 # -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'Portfolio'
 copyright = '2026, Peter Komaromy'
@@ -12,7 +7,6 @@ author = 'Peter Komaromy'
 release = '0.1'
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
    "sphinx.ext.githubpages",
@@ -24,7 +18,6 @@ exclude_patterns = []
 
 
 # -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'alabaster'
 html_static_path = ['_static']
